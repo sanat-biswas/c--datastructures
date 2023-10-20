6710d853dc51fae3b48c8ab6e5ab51ed13ecb5da
sanat-biswas <sanatbiswas786@gmail.com>
2023-10-20 22:15:39 +0530

    add: moved all the non zeroes to last of array

--- nonzeroes.cpp
+++ nonzeroes.cpp
@@ -4,27 +4,52 @@ using namespace std;
 
 vector<int> moveZeroes(vector<int> &arr, int n)
 {
-    vector<int> temp;
+    // vector<int> temp;
+
+    // for (int i = 0; i < n; i++)
+    // {
+
+    //     if (arr[i] != 0)
+    //     {
+    //         temp.push_back(arr[i]);
+    //     }
+    // }
+
+    // int nz = temp.size();
+
+    // for (int i = 0; i < nz; i++)
+    // {
+    //     arr[i] = temp[i];
+    // }
+
+    // for (int i = nz; i < n; i++)
+    // {
+    //     arr[i] = 0;
+    // }
+
+    // return arr;
+
+    int j = -1;
 
     for (int i = 0; i < n; i++)
     {
+        if (arr[i] == 0)
+        {
+            j = i;
+            break;
+        }  
+    }
 
+    if (j == -1)
+        return arr;
+
+    for (int i = j + 1; i < n; i++)
+    {
         if (arr[i] != 0)
         {
-            temp.push_back(arr[i]);
+            swap(arr[i], arr[j]);
+            j++;
         }
-    }
-
-    int nz = temp.size();
-
-    for (int i = 0; i < nz; i++)
-    {
-        arr[i] = temp[i];
-    }
-
-    for (int i = nz; i < n; i++)
-    {
-        arr[i] = 0;
     }
 
     return arr;
